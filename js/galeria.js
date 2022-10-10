038aa109aa8209d4454c78395990109924c8d92f
Roosveth <ryyatem@unal.edu.co>
2022-10-10 11:42:57 -0300

    Se agrega el proyecto

---
+++ js/galeria.js
@@ -0,0 +1,9 @@
+// <!------------------------- Función para el portafolio con Galería fotográfica   ------------------------->
+
+document.addEventListener('DOMContentLoaded', () => {
+	const imgLightBox = document.querySelectorAll('.materialboxed');
+	M.Materialbox.init(imgLightBox, {
+		inDuration: 500,
+		outDuration: 500
+	});
+});
